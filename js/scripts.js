b7411caa9260f42a87f1afd762770634112b663c
AJ Mcgladdery and Colin Cook <aj@>
2018-08-16 15:56:46 -0700

    Fix appending result text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 //back-end logic
 var symbols = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M","S"];
 var values = [1,4,5,9,10,40,50,90,100,400,500,900,1000,5000];
-var numerals = "";
-
 
 function romanNumerals(userInput) {
+  var numerals = "";
   if ((userInput < 1) || (userInput > 3999)) {
     alert("Please enter whole numbers between 1 and 3,999");
     return false;
@@ -17,13 +16,9 @@ function romanNumerals(userInput) {
           break;
         }
       }
-    }  return numerals;
+    } return numerals;
   }
 }
-
-
-
-
 
 //front-end logic
 $(document).ready(function() {
